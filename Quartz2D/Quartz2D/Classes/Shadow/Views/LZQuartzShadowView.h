08c3d5ee2182b4530d383adce99f5cc32ae14229
lucas <llzqingdao2012@gmail.com>
2016-08-29 11:08:59 +0800

    重构Main.storyboard。

---
+++ Quartz2D/Quartz2D/Classes/Shadow/Views/LZQuartzShadowView.h
@@ -0,0 +1,13 @@
+//
+//  LZQuartzShadowView.h
+//  Quartz2D
+//
+//  Created by lucas on 16/8/29.
+//  Copyright © 2016年 三只小猪. All rights reserved.
+//
+
+#import "LZQuartzView.h"
+
+@interface LZQuartzShadowView : LZQuartzView
+
+@end
---
+++ Quartz2D/Quartz2D/Classes/Shadow/Views/LZQuartzShadowView.m
@@ -0,0 +1,21 @@
+//
+//  LZQuartzShadowView.m
+//  Quartz2D
+//
+//  Created by lucas on 16/8/29.
+//  Copyright © 2016年 三只小猪. All rights reserved.
+//
+
+#import "LZQuartzShadowView.h"
+
+@implementation LZQuartzShadowView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
